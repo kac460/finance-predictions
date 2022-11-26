28f9abdd9a59c1d37f506679d4a7e2a867076f64
kac460 <46232247+kac460@users.noreply.github.com>
2022-11-26 16:26:44 -0500

    Refactor and add predictions tester (#1)
    * Move hardcoded predictions to separate python file
    
    * Light refactoring, add predictions_tester.py

---
+++ weights_functions.py
@@ -0,0 +1,11 @@
+# Returns reverse of [0.5, 0.25, 0.125, ..., 2^(-1*num_weights)]
+def exponential_weights(num_weights):
+    return [2**(-1*i) for i in reversed(range(1, num_weights+1))]
+
+
+def even_weights(num_weights):
+    return [1]*num_weights
+
+# Returns [num_weights, num_weights -1, ..., 1]
+def incrementing_weights(num_weights):
+    return list(range(1, num_weights+1))
