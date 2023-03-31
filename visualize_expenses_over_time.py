18d24889c883b948be7581a6ecd558ba3af6533f
kac460 <kac460@rutgers.edu>
2023-03-31 19:22:00 -0400

    Extract the month and year from filename

--- visualize_expenses_over_time.py
+++ visualize_expenses_over_time.py
@@ -1,2 +1,14 @@
-import matplotlib
+import matplotlib.pyplot as plt
+from categories import CATEGORIES
+from process_transactions import get_transaction_filenames, TRANSACTIONS_FILE_SUFFIX
+from os import path
+from datetime import date
 
+def main():
+    today = date.today()
+    for filename in get_transaction_filenames(today.year, today.month):
+        month_year = path.split(filename)[1].replace(TRANSACTIONS_FILE_SUFFIX, '')
+        print(month_year)
+
+if __name__ == '__main__':
+    main()
