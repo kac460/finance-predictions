aae8eb37a51b6ae1a9ee9f71e71fc58e18d0cb32
kac460 <kac460@rutgers.edu>
2023-03-31 20:19:32 -0400

    Visualize category expenses over time

--- visualize_expenses_over_time.py
+++ visualize_expenses_over_time.py
@@ -1,14 +1,58 @@
 import matplotlib.pyplot as plt
 from categories import CATEGORIES
-from process_transactions import get_transaction_filenames, TRANSACTIONS_FILE_SUFFIX
+from process_transactions import (
+    get_transaction_filenames, 
+    category_expenses_for_file,
+    total_expenses_in_file,
+    TRANSACTIONS_FILE_SUFFIX
+)
 from os import path
-from datetime import date
+from datetime import date, datetime
+
+_MONTH_YEAR_FORMAT = '%B %Y'
 
 def main():
     today = date.today()
+    dates: list[datetime] = []
+    total_monthly_expenses = []
+    category_monthly_expenses = {
+        category: []
+        for category in CATEGORIES
+    }
     for filename in get_transaction_filenames(today.year, today.month):
         month_year = path.split(filename)[1].replace(TRANSACTIONS_FILE_SUFFIX, '')
-        print(month_year)
+        file_date = datetime.strptime(month_year, _MONTH_YEAR_FORMAT)
+        dates.append(file_date)
+        total_monthly_expenses.append(total_expenses_in_file(filename))
+        category_expenses = category_expenses_for_file(filename)
+        for category in CATEGORIES:
+            category_monthly_expenses[category].append(
+                max(category_expenses.get(category, 0), 0)
+            )
+    #plt.plot(dates, total_monthly_expenses)
+    categories_sorted = sorted(
+        CATEGORIES, 
+        key=lambda cat: sum(category_monthly_expenses[cat]),
+        reverse=True
+    )
+    for category in categories_sorted:
+        plt.plot(
+            dates, 
+            category_monthly_expenses[category], 
+            label=category
+        )
+    plt.title('Expenses Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Expense ($)')
+    plt.legend()
+    date_ticks = dates[::1]
+    plt.xticks(
+        date_ticks, 
+        [dt.strftime('%m-%Y') for dt in date_ticks],
+        rotation=45
+    )
+    plt.show()
+
 
 if __name__ == '__main__':
     main()
