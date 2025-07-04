19450c9a5b02c14a20d9e9bb65f3b763fc5c45dc
kac460 <46232247+kac460@users.noreply.github.com>
2025-07-04 11:14:09 -0400

    Add new func (#20)

--- weights_functions.py
+++ weights_functions.py
@@ -1,15 +1,28 @@
 # Returns reverse of [0.5, 0.25, 0.125, ..., 2^(-1*num_weights)]
-def exponential_weights(num_weights):
-    return [2**(-1*i) for i in reversed(range(1, num_weights+1))]
+def exponential_weights(num_weights: int) -> list[float]:
+    return [2 ** (-1 * i) for i in reversed(range(1, num_weights + 1))]
 
 
-def even_weights(num_weights):
-    return [1]*num_weights
+def even_weights(num_weights: int) -> list[float]:
+    return [1] * num_weights
+
+
+_MONTHS_IN_YEAR = 12
+
+
+def even_weights_last_year_only(num_weights: int) -> list[float]:
+    if num_weights <= _MONTHS_IN_YEAR:
+        return even_weights(num_weights)
+    ignore = [0] * (num_weights - _MONTHS_IN_YEAR)
+    use = [1] * _MONTHS_IN_YEAR
+    return ignore + use
+
 
 # Returns [1, 2, ..., num_weights]
-def incrementing_weights(num_weights):
-    return list(range(1, num_weights+1))
+def incrementing_weights(num_weights: int) -> list[float]:
+    return list(range(1, num_weights + 1))
+
 
 def weighted_avg(vals, weights):
     assert len(vals) == len(weights)
-    return sum([vals[i]*weights[i] for i in range(len(vals))])/sum(weights)
+    return sum([vals[i] * weights[i] for i in range(len(vals))]) / sum(weights)
